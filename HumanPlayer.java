aee8249fe710e4e1afecf34613423dd743e9fd0d
www.milazar@mail.ru <www.milazar@mail.ru@a9142878-3d2d-e4c5-aa6c-9241c77b8fa9>
2010-12-12 06:14:33 +0000

    Отредактировал под ваши комментарии, но не получилось сделать тот, насчет которого я вас спрашивал

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,8 +1,25 @@
 package krestnol;
 class HumanPlayer{    
-    public String sign;
-    public String name;
-    HumanPlayer(String sg){
-        sign = sg;        
+    private String sign;
+    private String name;
+
+    public void setName(String nik)
+    {
+        name = nik;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setSign(String XorO)
+    {
+        sign = XorO;
+    }
+
+    public String getSign()
+    {
+        return sign;
     }
 }
