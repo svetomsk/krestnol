45c765646191f83e5c8e2b02bf55f4b8959b80b1
www.milazar@mail.ru <www.milazar@mail.ru@a9142878-3d2d-e4c5-aa6c-9241c77b8fa9>
2011-01-10 15:18:35 +0000

    Простите, забыл добавить новый класс

---
+++ GetText.java
@@ -0,0 +1,87 @@
+package krestnol;
+import java.io.*;
+class GetText
+{
+    private BufferedReader bufR;
+    private int kol = 14;
+    private String [] value = new String[kol];
+
+    GetText()throws IOException
+    {
+       bufR = new BufferedReader(new FileReader("text.txt"));
+       for(int i = 0; i < kol; i++)
+       {
+           value[i] = bufR.readLine();
+       }
+    }
+
+    public String name1()
+    {
+        return value[0];
+    }
+
+    public String sign1()
+    {
+        return value[1];
+    }
+
+    public String name2()
+    {
+        return value[2];
+    }
+
+    public String sign2()
+    {
+        return value[3];
+    }
+
+    public String frameName()
+    {
+        return value[4];
+    }
+
+    public String fail()
+    {
+        return value[5];
+    }
+
+    public String igra()
+    {
+        return value[6];
+    }
+
+    public String spravke()
+    {
+        return value[7];
+    }
+
+    public String vyxod()
+    {
+        return value[8];
+    }
+
+    public String novaya()
+    {
+        return value[9];
+    }
+
+    public String obigre()
+    {
+        return value[10];
+    }
+
+    public String anyone()
+    {
+        return value[11];
+    }
+
+    public String html()
+    {
+        return value[12];
+    }
+
+    public String win()
+    {
+        return value[13];
+    }
+}
