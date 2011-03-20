4e549f4431efc89c6f1ca342961190f49b03dce1
www.milazar@mail.ru <www.milazar@mail.ru@a9142878-3d2d-e4c5-aa6c-9241c77b8fa9>
2011-03-20 04:13:42 +0000

    Добавил интерфейс IPlayer и класс ComputerPlayer, который будет выполнять роль компьютера-игрока. Пока что организацию ходов наладить не удалось, но скоро постараюсь сделать

---
+++ trunk/ComputerPlayer.java
@@ -0,0 +1,51 @@
+package krestnol;
+class ComputerPlayer implements IPlayer
+{
+    private String sign;
+    private String name;
+    private Model m;
+    private View w;
+    private boolean value;
+    ComputerPlayer(Model m, View w)
+    {
+        sign = "X";
+        name = "Computer";
+        this.m = m;
+        this.w = w;
+    }
+
+    public void setSign(String value){}
+
+    public void setName(String value){}
+
+    public String getSign()
+    {
+        return sign;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void hod()
+    {
+        w.delListeners();
+        for(int i = 0; i < 3; i++)
+        {
+            for(int g = 0; g < 3; g++)
+            {
+                if(m.getF(i, g) == true)
+                {
+                    w.setButtonText("O", i, g);
+                }
+            }
+        }
+        w.addListeners();
+    }
+
+    public boolean notifyIP()
+    {
+        return value;
+    }
+}
